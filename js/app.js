15e06b36e953d41d60d1032964ae4e2b2828faa0
Antwar <110550543+Antwar@users.noreply.github.com>
2026-02-21 15:51:10 +0500

    faq js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const heroBody = document.querySelector('.hero__body');
     const dots = document.querySelectorAll('.hero__dot');
+    const accordionItems = document.querySelectorAll('.accordion__item');
     
     let currentSlide = 0;
 
+    // Смена заднего фона в начале страницы по клику
     function changeSlide(index) {
 
         heroBody.classList.remove('hero__body--slide-1', 'hero__body--slide-2', 'hero__body--slide-3');
-        
         heroBody.classList.add(`hero__body--slide-${index + 1}`);
 
         dots.forEach(dot => dot.classList.remove('hero__dot--active'));
@@ -19,6 +20,19 @@ document.addEventListener('DOMContentLoaded', () => {
     dots.forEach((dot, index) => {
         dot.addEventListener('click', () => {
             changeSlide(index);
+        });
+    });
+
+    // Автоматическое закрытие активной карточки вопроса по клику на другой
+    accordionItems.forEach((item) => {
+        item.addEventListener('toggle', () => {
+            if (item.open) {
+                accordionItems.forEach((otherItem) => {
+                    if (otherItem !== item && otherItem.open) {
+                        otherItem.open = false;
+                    }
+                });
+            }
         });
     });
 
